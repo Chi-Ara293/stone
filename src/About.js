189b44d5dfd4d329d9a5618775d152b422510463
Chigusa <181899584+Chi-Ara293@users.noreply.github.com>
2026-08-19 08:24:50 -0400

    change about page

--- src/About.js
+++ src/About.js
@@ -3,13 +3,12 @@ import chairc from './assets/chairc.jpg'
 const About = () => {
   return (
     <div>
-      <h2 className='about-me'>About me</h2>
+      <h2 className='about-me'>About</h2>
       <div className='pAndImg'>
         <div>
-          <p className='p1'>a freelance frontend developer<br />
-            (currently working on learning backend too)</p>
-          <p className='p2'>a poet</p>
-          <p className='p3'>a part-time worker</p>
+          <p className='p1'>Lorem ipsum sit elit amet, consectetur  dolor adipiscing , sed  eiusmod  ut tempor incididunt labore et dolore do magna aliqua.</p>
+          <p className='p2'>Lorem ipsum dolor sit, consectetur adipiscing elit, do eiusmod magna tempor incididunt  aliqua ut  et dolore sed labore.</p>
+          <p className='p3'>Lorem ipsum et et magna aliqua dolore magna et dolore aliquadolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
         </div>
         <img className='aboutImage' src={chairc} alt='chair drawing' />
       </div>
